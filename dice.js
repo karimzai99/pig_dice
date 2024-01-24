d1ebc1f51ed62eb641aac4bfd5a36ffd6da37d67
Naseer Karimzai <karimzai99@gmail.com>
2024-01-24 17:50:33 -0500

    removed images directory and made some changes in btn hold

--- dice.js
+++ dice.js
@@ -1,4 +1,3 @@
-// Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
@@ -18,14 +17,14 @@ const init = function () {
   scores = [0, 0];
   currentScore = 0;
   activePlayer = 0;
-
+  playing = true;
 
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  diceEl.classList.add('hidden')
+  diceEl.classList.add('hidden');
   player0El.classList.remove('player--winner');
   player1El.classList.remove('player--winner');
   player0El.classList.add('player--active');
@@ -33,39 +32,75 @@ const init = function () {
 };
 init();
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+//  switching player condition.
+if (activePlayer === 0) {
+    activePlayer = 1;
+} else {
+    activePlayer = 0;
+}
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // if the playing game is true 
+  if (playing) { 
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1 game will switch to next player.
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice; // what ever the current value of dice is will be add to the current score
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
 
 
 
-// rolling dice 
-btnRoll.addEventListener('click', function(){
-
-        // Random dice generator
-        let dice = Math.floor(Math.random() * 6) + 1;
-
-        const diceEl = document.querySelector('.dice');
-        // hide dice // TODO
-        diceEl.classList.remove('hidden');
 
 
-        // generetes random dice pictures. 
-        diceEl.src = `images/dice-${dice}.png`
-    
-        if (dice !== 1) {
-            currentScore += dice;
-
-            // add current dice to current score
-            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        }
-        else {
-            // the player should be switch 
-        }
-})
-
-// button hold 
-    btnHold.addEventListener('click', function(){
-        console.log('hello');
-    })
-// button new game 
-
-
-// switch player 
